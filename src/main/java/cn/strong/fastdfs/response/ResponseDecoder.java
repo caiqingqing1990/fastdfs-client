fb719ef4488d0c8c6a702122281638ef5cee3edf
liulongbiao <liulongbiao@cnstrong.cn>
2015-04-30 16:11:35 +0800

    add ResponseDecoder

---
+++ src/main/java/cn/strong/fastdfs/response/ResponseDecoder.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package cn.strong.fastdfs.response;
+
+import io.netty.buffer.ByteBuf;
+
+/**
+ * 响应解码器
+ * 
+ * @author liulongbiao
+ *
+ */
+public interface ResponseDecoder<T> {
+
+	/**
+	 * 期待的长度值，小于 0 时不验证
+	 * 
+	 * @return
+	 */
+	long expectLength();
+
+	/**
+	 * 解码响应
+	 * 
+	 * @param buf
+	 * @return
+	 */
+	T decode(ByteBuf buf);
+}
