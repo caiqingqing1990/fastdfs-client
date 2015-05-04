f6a19820129dfc069beed435195863280f058be4
liulongbiao <liulongbiao@cnstrong.cn>
2015-05-04 14:01:28 +0800

    update StoragePathRequest

--- src/main/java/cn/strong/fastdfs/request/tracker/StoragePathRequest.java
+++ src/main/java/cn/strong/fastdfs/request/tracker/StoragePathRequest.java
@@ -3,15 +3,8 @@
  */
 package cn.strong.fastdfs.request.tracker;
 
-import static cn.strong.fastdfs.core.Consts.FDFS_GROUP_LEN;
-import static cn.strong.fastdfs.core.Helpers.requireNonNull;
-import static cn.strong.fastdfs.core.Helpers.writeFixLength;
-import static io.netty.util.CharsetUtil.UTF_8;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
-
 import cn.strong.fastdfs.model.StoragePath;
-import cn.strong.fastdfs.request.Request;
+import cn.strong.fastdfs.request.AbstractStoragePathRequest;
 
 /**
  * 发送内容为存储路径的请求
@@ -19,30 +12,18 @@ import cn.strong.fastdfs.request.Request;
  * @author liulongbiao
  *
  */
-public class StoragePathRequest implements Request {
+public class StoragePathRequest extends AbstractStoragePathRequest {
 
-	private final StoragePath spath;
 	private final byte cmd;
 
 	public StoragePathRequest(StoragePath spath, byte cmd) {
-		this.spath = requireNonNull(spath);
+		super(spath);
 		this.cmd = cmd;
 	}
 
 	@Override
 	public byte cmd() {
 		return cmd;
-	}
-
-	@Override
-	public long length() {
-		return FDFS_GROUP_LEN + spath.path.getBytes(UTF_8).length;
-	}
-
-	@Override
-	public void writeBody(ByteBuf buf) {
-		writeFixLength(buf, spath.group, FDFS_GROUP_LEN);
-		ByteBufUtil.writeUtf8(buf, spath.path);
 	}
 
 }
